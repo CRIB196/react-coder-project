9f56b92fa84c67804c0f933bd7099008eeb63747
Camila Riboldi <cami.riboldi@gmail.com>
2023-03-24 17:39:47 -0300

    -cart class 6

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,5 +1,7 @@
+import { Button } from "@mui/material";
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import Swal from "sweetalert2";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -7,10 +9,28 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
 const Cart = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, clearCart, getTotalPrice, deleteProductById } =
+    useContext(CartContext);
 
-  return (
-    <div>
+  const clear = () => {
+    Swal.fire({
+      title: "Are you sure you want to empty cart?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Yes, empty cart",
+      denyButtonText: `No, do not empty cart`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clearCart();
+        Swal.fire("Cart succesfully empty", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Cart stays the same way as before", "", "info");
+      }
+    });
+  };
+
+ return (
+   <div>
       <Typography gutterBottom variant="h5" component="div">
         Shopping cart
       </Typography>
@@ -46,7 +66,32 @@ const Cart = () => {
                 alt="Live from space album cover"
               />
             </Card>
+            <Button
+                  color="secondary"
+                  variant="contained"
+                  onClick={() => deleteProductById(element.id)}
+                >
+                  Delete
+                </Button>
+              
+               <div className="cart-info">
+        <h3>Total price: {getTotalPrice()}</h3>
+        <h3>Discount: - </h3>
+        <h3>Final Price: -</h3>
+
+        {cart.length > 0 && (
+          <div className="btn-cart">
+            <Button variant="contained">Comprar</Button>
+            <Button onClick={clear} variant="contained">
+              Empty Cart
+            </Button>
           </div>
+        )}
+
+        <h1>Total of Cart ${getTotalPrice()}</h1>
+      </div>
+          </div>
+          
         );
       })}
     </div>
