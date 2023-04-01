bc6e4aa796b8ce27fcaa81dbb8eedc96a644d3ae
Camila Riboldi <cami.riboldi@gmail.com>
2023-04-01 14:19:05 -0300

    -fixing style

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Cart from "./Components/Cart/Cart";
 import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
 import Form from "./Components/Form/Form";
 import CartContextProvider from "./context/CartContext";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <BrowserRouter>
       <CartContextProvider>
-        <div className="body-classname" >
+        <div className="body-classname">
           <Navbar />
           <Routes>
             <Route path="/" element={<ItemListContainer />} />
@@ -26,7 +27,23 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/form" element={<Form />} />
 
-            <Route path="*" element={<h1> error 404: Not found </h1>} />
+            <Route
+              path="*"
+              element={
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  color="text.secondary"
+                  style={{
+                    marginTop:"2%",
+                    marginLeft:"2%",
+                  }}
+                >
+                  error 404: Not found  <br/> Please if your are searching for an item, this is not an existing current id. 
+                </Typography>
+              }
+            />
           </Routes>
         </div>
         <Footer />
